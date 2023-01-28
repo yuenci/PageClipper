5fa784482c96115204ae3d8a676cfa08df72e4d6
Innis <1575270674@qq.com>
2023-01-28 17:04:27 +0800

    feat: UI fream finish

---
+++ src/components/Login.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    name: "Login",
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+
+    },
+}
+</script>
+<style lang="">
+    
+</style>
